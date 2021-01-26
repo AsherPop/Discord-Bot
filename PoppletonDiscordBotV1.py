39225f76c87b44aab997da8800e27d11c513740a
AsherPop <77747352+AsherPop@users.noreply.github.com>
2021-01-26 11:03:39 -0800

    Add files via upload

--- PoppletonDiscordBotV1.py
+++ PoppletonDiscordBotV1.py
@@ -1,11 +1,10 @@
 import discord
-import asyncio
 import random
-from discord.ext.commands import Bot
+import asyncio
 from discord.ext import commands
 
 
-pp = commands.Bot(command_prefix = '!')
+pp = commands.Bot(command_prefix = '-')
 
 
 @pp.event
@@ -30,23 +29,7 @@ async def on_member_remove(member):
     if not member.bot:
         await get.channel("general").send(f'{member.display_name} is no longer in the server!')
 
-
-@pp.event
-async def on_member_join(member):
-    print("Recognised that a member called " + member.name + " joined")
-    await pp.send_message(member, newUserDMMessage)
-    await pp.send_message(discord.Object(id='CHANNELID'), 'Welcome!')
-    print("Sent message to " + member.name)
-    print("Sent message about " + member.name + " to #CHANNEL")
-
-
-@pp.event
-async def on_member_remove(member):
-    print("Recognised that a member called " + member.name + " left")
-    await pp.send_message(discord.Object(id='CHANNELID'), member.name + ' left')
-    print("Sent message to #CHANNEL")
-
-
+        
 @pp.command()
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
@@ -60,4 +43,4 @@ async def ping(ctx):
 
 
 
-pp.run('ODAxODM0MTA1ODA0ODE2Mzk0.YAmcNA.Af3aOaVlb2zz55ZZHtE9GU3IZ54')
+pp.run('ODAxODM0MTA1ODA0ODE2Mzk0.YAmcNA.uyz1FI7FtRkeimD7_j3ApQQn_vY')
